005e94bbe980596715bd89b20a85575db286796b
Vilkiy <martynex2@gmail.com>
2026-04-21 14:19:37 +0300

    Added Sword Character
    Alternating swings and allat

--- logic/game_handler/game_handler.gd
+++ logic/game_handler/game_handler.gd
@@ -1,8 +1,7 @@
 class_name GameHandler
+
 extends Node
-
 const GAME_OVER_SCREEN = preload("res://logic/UI/game_over.tscn")
-
 func _ready() -> void:
 	GlobalVar.game_handler = self
 	var player_scene = load(GameConfig.selected_hero)
@@ -10,7 +9,6 @@ func _ready() -> void:
 	add_child(player)
 	await get_tree().process_frame
 	GlobalVar.player.health_handler.died.connect(_on_player_died)
-
 func _on_player_died() -> void:
 	get_tree().paused = true
 	await get_tree().create_timer(1.0).timeout;
